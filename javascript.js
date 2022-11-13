93a5361b9d4ee794d70e56ca070fed6af30ce192
russellella <116303658+russellella@users.noreply.github.com>
2022-11-13 10:48:54 -0800

    Fix issues in playRound function
    -Fixed bug causing "oops" messages due to unnecessary repeats of the
    getComputerSelection function
    -Simplified randomized computerSelection function
    -Added multiple tie options to help confirm playRound working
    -Organized code
    -Confirmed that all code up to this point is working as intended

--- javascript.js
+++ javascript.js
@@ -1,56 +1,55 @@
+
 // PlayerInput prompt - WORKS
 let playerInput = prompt("Type rock, paper, or scissors")
-
-// Random computer selection - numerical value - WORKS
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
-  }
-
-let result = getRndInteger(1, 4);
-
-// Converts computer numerical value to selection - WORKS
-if (result == 1) {
-    computerSelection = "rock";
-  } else if (result == 2) {
-    computerSelection = "paper";
-  } else {
-    computerSelection = "scissors";
-  }
-
-/* DISABLED for testing
-const playerInput = "Rock";
-*/
 
 // Makes playerSelection lowercase - WORKS
 const playerSelection = playerInput.toLowerCase();
 
-// Plays a round and returns a message - WORKS
-
-function playRound(playerSelection, computerSelection) {
-  if ((playerSelection == "rock") && (computerSelection == "scissors")) {
-    return "You win! Rock beats Scissors.";
-  } else if ((playerSelection == "rock") && (computerSelection == "paper")) {
-    return "You lose! Paper beats Rock.";
-  } else if ((playerSelection == "paper") && (computerSelection == "scissors")) {
-    return "You lose! Scissors beats paper.";
-  } else if ((playerSelection == "paper") && (computerSelection == "rock")) {
-    return "You win! Paper beats rock.";
-  } else if ((playerSelection == "scissors") && (computerSelection == "rock")) {
-    return "You lose! Rock beats scissors.";
-  } else if ((playerSelection == "scissors") && (computerSelection == "paper")) {
-    return "You win! Scissors beats paper.";
-  } else { return "It's a tie!";
+// Returns Computer Selection - WORKS
+function getComputerSelection() {
+  let result = Math.floor(Math.random() * 3);
+  if (result === 0) {
+    return "rock";
+  } else if (result === 1) {
+    return "paper";
+  } else if (result === 2) {
+    return "scissors";
+  } else {
+    return "uh oh";
   }
 }
 
-console.log(playRound(playerSelection, computerSelection));
-
-/* Testing Area 
-function game(playRound) {
-  for (let i = 0; i < 5; i++) {
-    console.log(playRound(playerSelection, computerSelection));
+// Plays 1 Round of RPS - WORKS
+// CHECK IF TIE COUNTS AS ROUND
+function playRound() {
+  let computerSelection = getComputerSelection();
+  if ((playerSelection == "rock") && (computerSelection == "scissors")) {
+    return "You win this round! Rock beats Scissors.";
+  } else if ((playerSelection == "rock") && (computerSelection == "paper")) {
+    return "You lose this round! Paper beats Rock.";
+  } else if ((playerSelection == "paper") && (computerSelection == "scissors")) {
+    return "You lose this round! Scissors beats paper.";
+  } else if ((playerSelection == "paper") && (computerSelection == "rock")) {
+    return "You win this round! Paper beats rock.";
+  } else if ((playerSelection == "scissors") && (computerSelection == "rock")) {
+    return "You lose this round! Rock beats scissors.";
+  } else if ((playerSelection == "scissors") && (computerSelection == "paper")) {
+    return "You win this round! Scissors beats paper.";
+  } else if (((playerSelection == "scissors") && (computerSelection == "scissors"))
+          || ((playerSelection == "rock") && (computerSelection == "rock"))
+          || ((playerSelection == "paper") && (computerSelection == "paper"))) {
+    return "It's a tie!";
+  } else {
+    return "Oops, that doesn't work!";
   }
+}
+
+
+/*
+function game() {
+  for (let i = 0; i < 5; i++) {
+  playRound();
+  
+}
 }
 */
-
-
